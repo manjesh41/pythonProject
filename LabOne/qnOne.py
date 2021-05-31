4acd8fdb7a42af222512555232945b47485557c9
ACER <manjeshsilwal@gmail.com>
2021-05-31 09:48:08 +0545

    qnOne commit

---
+++ LabOne/qnOne.py
@@ -0,0 +1,9 @@
+'''
+write a program that takes three numbersand print their sum sum.
+every number is given on seperate line
+'''
+numOne = 10
+numTwo =20
+numThree =30
+sum =numOne+numTwo+numThree
+print(sum)
---
+++ lab projrct/area.py
@@ -0,0 +1,3 @@
+'''
+write a program that that readds the length of the base and the height of a right-angled triangle and print the area.every number is given on a seperate line.
+'''
