1ae028c2df0afccd6426414d472e25f6a5ba375f
ACER <manjeshsilwal@gmail.com>
2021-05-31 10:05:51 +0545

    program for area file commit

--- lab projrct/area.py
+++ lab projrct/area.py
@@ -1,3 +1,7 @@
 '''
-write a program that that readds the length of the base and the height of a right-angled triangle and print the area.every number is given on a seperate line.
+write a program that  reads the length of the base and the height of a right-angled triangle and print the area.every number is given on a seperate line.
 '''
+l =10
+h =20
+area = (l*h)/2
+print(f"the area of right angle triangle is{area}")
